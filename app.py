d215e77ea50dbfcd19c21a2be0a0955c5cb02d1b
aflanders7 <audrey.flanders117@gmail.com>
2023-11-01 08:42:55 -0700

    insert complete

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 from flask import Flask, render_template, json, redirect, request
 import os
-import database.db_connector as db
 from flask_mysqldb import MySQL
-from db_connector import connect_to_database, execute_query
+from database.db_connector import connect_to_database, execute_query
+from dotenv import load_dotenv, find_dotenv
 
-db_connection = db.connect_to_database()
+load_dotenv(find_dotenv())
+
+host = os.environ.get("host")
+user = os.environ.get("user")
+passwd = os.environ.get("passwd")
+db = os.environ.get("db")
 
 # Configuration
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = host
+app.config['MYSQL_USER'] = user
+app.config['MYSQL_PASSWORD'] = passwd
+app.config['MYSQL_DB'] = db
 mysql = MySQL(app)
+
 
 # Routes 
 
@@ -21,8 +32,9 @@ def root():
 def expense():
     if request.method == "GET":
         query = "SELECT Name, Amount, Category, Description, Day FROM Expenses;"
-        cursor = db.execute_query(db_connection=db_connection, query=query)
-        data = cursor.fetchall()
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
         return render_template("expenses.j2", Expenses=data)
 
     if request.method == "POST":
@@ -34,8 +46,10 @@ def expense():
             Day = request.form["date"]
 
             query = "INSERT INTO Expenses (Name, Amount, Category, Description, Day) VALUES (%s, %s, %s, %s, %s)"
-            
-            db.execute(query, (Name, Amount, Category, Description, Day))
+
+            cur = mysql.connection.cursor()
+            cur.execute(query, (Name, Amount, Category, Description, Day))
+            mysql.connection.commit()
         
         return redirect("/expenses")
 
